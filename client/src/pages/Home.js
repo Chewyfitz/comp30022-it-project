3015f9692dc2aac16c35575567f674c133b3605c
rowcla <31002885+rowcla@users.noreply.github.com>
2019-09-23 18:32:18 +1000

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -41,8 +41,12 @@ class FileInput extends React.Component {
     alert(
       `Selected file - ${
         this.fileInput.current.files[0].name
-      }`
+      }
+      `
     );
+    axios.post('https://rowan-api-test-enviro.herokuapp.com/api/upload', {
+			this.fileInput.current.files;
+		});
   }
 
   render() {
