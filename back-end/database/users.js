306a5d04b667bde52b85b5dd958f37ac7f315ec0
PCA000 <pca0000@gmail.com>
2019-09-28 20:40:29 +1000

    Test to see if I can push now
    I've removed some coments that  had @ in them just to see if they were what was stopping me from pushing to comitments.

--- back-end/database/users.js
+++ back-end/database/users.js
@@ -5,9 +5,6 @@ const userFields = {
     password: "Password Hash",
 };
 
-/**Adds a User collection document with userID as the document key and password as a stored value
- * @param userId key of the document
- * @param password stored value in the document*/
 function addUser(userId, password) {
     let data = {};
     data[userFields.password] = password;
@@ -15,9 +12,6 @@ function addUser(userId, password) {
     general.addDataToDoc(data, usersPath(), userId);
 }
 
-/**Updates the given users password with the supplied password
- * @param userID the key of the document to update
- * @param password the new value of the passwordHash field in the documnet*/
 function updateUserPassword(userID, password=null){
     let data = {};
     if(password != null){
