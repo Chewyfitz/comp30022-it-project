9830e3da26bfe86d772f167b8d535a41c120a54c
rowcla <31002885+rowcla@users.noreply.github.com>
2019-10-07 14:13:41 +1100

    Update UploadButton.js

--- client/src/components/layout/UploadButton.js
+++ client/src/components/layout/UploadButton.js
@@ -17,16 +17,10 @@ class UploadButton extends React.Component {
     }
     onClickHandler = () => {
        const data = new FormData() 
-       console.log("starting file parsing");
+       data.append('user', 'testuser');
        for(var x = 0; x<this.state.selectedFile.length; x++) {
-	   console.log("file parsed");
-	       console.log(this.state.selectedFile[x]);
            data.append('file', this.state.selectedFile[x])
        }
-	    console.log("all parsed");
-	    console.log(data);
-	    console.log('test');
-	    console.log(data.getAll('file'));
         axios.post("https://rowan-api-test-enviro.herokuapp.com/api/image", data, { })
         .then(res => { // then print response status
           console.log(res.statusText)
