ed2b09b53afebade4220ffdd5de97bc7ffe18213
Chewyfitz <chewyfitz@gmail.com>
2019-10-08 00:35:18 +1100

    Back-end GCS upload (#7)
    * Adding Google Cloud Storage photo uploads (thanks to Rowan!)
    
    * Updated a little bit, still yet to test.
    
    * Fiddling with return values - Beware: GCS initialisation creates a file that MUST NOT be added to the git
    
    * cleanup, reorganised the file upload so it happens concurrently instead of sequentially.
    
    * Update index.js
    
    * Create UploadButton.js
    
    * Update index.js
    
    * Update index.js (+1 squashed commits)
    
    Squashed commits:
    
    [439e43b] Create UploadButton.js (+1 squashed commits)
    
    Squashed commits:
    
    [67f3397] Update index.js
    
    * Reverse mistaken commit
    
    * Reverse Mistaken Commit (again because git weirdness)

--- back-end/util/image.js
+++ back-end/util/image.js
@@ -1,4 +1,19 @@
+// ============================================================================
+// Libraries
 const database = require('../database.js');
+
+// Google Cloud Vision to label images (not yet implemented)
+var vision = require('@google-cloud/vision');
+
+// Google Cloud Storage to store uploaded images
+const { Storage } = require('@google-cloud/storage');
+// config is the result of some extra code to parse a JSON environment variable
+var config = require('../config/gcsConfig');
+
+const gcs = new Storage(config());
+
+// ============================================================================
+// Functions
 
 async function getImageById(user, imageId){
     console.log("getImageById: "+imageId);
@@ -21,8 +36,26 @@ async function getImageRefById(user, imageId){
  * @param {String} photoDateTime - datetime as string
  */
 async function addPhotoToUser(user, image, photoDateTime = null){
+    // ... pretty much just call the database function
     success = database.addPhoto(user, image, photoDateTime);
     return success;
+}
+
+// TODO: add photoDateTimes
+// TODO: change successes to document IDs
+async function addPhotosToUser(user, images){
+    var successes = []
+    console.log("addPhotosToUser: "+images);
+    // Iterate through each image and add it individually
+    for(i in images){
+        console.log(`image: ${images[i]}`);
+        await addPhotoToUser(user, images[i]).then(imageref => {
+            // return an id for each image input attempt
+            successes.push(imageref);
+        })
+    }
+
+    return successes;
 }
 
 // TODO: Implement Deletions
@@ -30,9 +63,51 @@ async function deletePhotoById(user, image){
     console.log("Err: Incomplete Function deletePhotoById");
 }
 
+// Thanks to Rowan for figuring out how all this works!
+async function uploadPhotos(files){
+    // Set the information for the uploaded file
+    var bucket = gcs.bucket(`gs://${process.env.PROJECTID}.appspot.com`);
+    var strings = [];
+    for(i in files){
+        // filename -- include username?
+        const gcsname = `${Date.now()}-${files[i].originalname}`;
+        const file = bucket.file(gcsname); // prepare the file
+        const stream = file.createWriteStream({ // Set the stream config
+            metadata: {
+                contentType: files[i].mimetype
+            },
+            resumable: false
+        });
+        // Log any errors in the upload stream
+        stream.on('error', err => {
+            // Not sure what this does
+            files[i].cloudStorageError = err;
+            console.error(err);
+        });
+
+        // add the reference for the file to the list
+        const uri = `https://storage.googleapis.com/${bucket.name}/${gcsname}`
+        strings[i] = uri;
+        console.log(`URI ${i}: ${uri}`);
+
+        // make the file public when the stream finishes
+        stream.on('finish', async () => {
+            file.makePublic();
+        });
+
+        // end the stream
+        stream.end(files[i].buffer);
+    }
+
+    // Return the list of URIs
+    return strings;
+}
+
 module.exports = {
+    addPhotoToUser,
+    addPhotosToUser,
+    deletePhotoById,
     getImageById,
     getImageRefById,
-    addPhotoToUser,
-    deletePhotoById
+    uploadPhotos
 }
