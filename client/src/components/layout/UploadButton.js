930d1dd27265d41b562af8fe1263aca985349adc
rowcla <31002885+rowcla@users.noreply.github.com>
2019-10-07 13:59:22 +1100

    Update UploadButton.js

--- client/src/components/layout/UploadButton.js
+++ client/src/components/layout/UploadButton.js
@@ -1,3 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 class UploadButton extends React.Component {
     constructor(props) {
       super(props);
