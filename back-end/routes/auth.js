fc55a2d558a9faf9f61f48b2030fe6fe854f98ee
Chewyfitz <Chewyfitz@Gmail.com>
2019-10-17 23:53:31 +1100

    Added login route - uses the authorization header

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -9,13 +9,32 @@ const util = require('../util/auth');
 router.post(['/register', '/signup'], (req, res, next) => {
     // Create a user - auth optional
     util.register().then((user) => {
-		res.send(user);
-	});
-    // res.sendStatus(500);
+        res.status(201);
+        res.send(user);
+	}).catch(err => {
+        res.status(500);
+        res.send(err);
+    });
 });
 
 router.post('/login', (req, res, next) => {
     // Log a user in - auth optional
+
+    // get the login (email) and password from the auth header
+    [login, password] = util.decode(req.headers.authorization);
+
+    // Try to sign the user in with their details
+    util.signIn(login, password).then((user) => {
+        if(user){
+            res.status(200);
+            res.send(user);
+        } else {
+            res.sendStatus(404);
+        }
+    }).catch(err => {
+        res.status(500);
+        res.send(err);
+    });
     res.sendStatus(500);
 });
 
--- back-end/util/auth.js
+++ back-end/util/auth.js
@@ -2,14 +2,26 @@ const database = require('../database.js');
 const firebase = require('firebase');
 require('firebase/auth');
 
-async function register(){
-    // fbAuth.createUserWithEmailAndPassword('email@example.com', 'password');
-    user = await firebase.auth().createUserWithEmailAndPassword('email@example.com', 'password');
+async function register(email, password){
+    user = await firebase.auth().createUserWithEmailAndPassword(email, password);
 
+    return user;
+}
+
+function decode(authString){
+    const b64auth = (authString || '').split(' ')[1] || '';
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+    return [login, password];
+}
+
+async function signIn(email, password){
+    user = await firebase.auth().signInWithEmailAndPassword(email, password)
     return user;
 }
 
 const val = 0;
 module.exports = {
+    decode,
     register,
+    signIn,
 }
