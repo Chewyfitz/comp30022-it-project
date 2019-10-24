b473eea1c8525839690f9a3c926beefb0e507fd0
PCA000 <pca0000@gmail.com>
2019-10-25 02:57:59 +1100

    stops requiring all of firebase, should still work, see 2860c2d

--- back-end/util/user.js
+++ back-end/util/user.js
@@ -1,6 +1,6 @@
 const database = require('../database.js');
-const firebase = require('firebase');
-require('firebase/auth');
+//const firebase = require('firebase');
+const auth = require('firebase/auth');
 
 ////////////////////////////////////////////////////////////////////////////////
 //                                   CREATE                                   //
@@ -11,13 +11,13 @@ require('firebase/auth');
 ////////////////////////////////////////////////////////////////////////////////
 
 async function checkToken(token){
-    success = await firebase.auth().checkToken(token);
+    success = await auth.checkToken(token);
     return success;
 }
 
 async function getUserInfo(token, userId){
     if(checkToken(token)){
-        userinfo = await firebase.auth().getUserInfo(userId);
+        userinfo = await auth.getUserInfo(userId);
         return userinfo;
     } else {
         throw new Error("Invalid token");
@@ -29,12 +29,12 @@ async function getUserInfo(token, userId){
 ////////////////////////////////////////////////////////////////////////////////
 
 async function send_password_reset_email(email){
-    success = await firebase.auth().sendPasswordResetEmail(email);
+    success = await auth.sendPasswordResetEmail(email);
     return success;
 }
 
 async function reset_password(code, newPassword){
-    success = await firebase.auth().confirmPasswordReset(code, newPassword);
+    success = await auth.confirmPasswordReset(code, newPassword);
     return success;
 }
 
